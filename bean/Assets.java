badc6816b492040bfab4184fcca11e04ad184a45
wobias12 <tobias.weiss1@gmx.de>
2023-06-02 19:13:37 +0200

    Aenderungen im Investment Funktionieren aber noch nicht
    Assets.java ist neu , InvestementsServlet.java

---
+++ bean/Assets.java
@@ -0,0 +1,45 @@
+package stacked_bs.bean;
+
+import java.io.Serializable;
+
+public class Assets implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private String username;
+	private String stockname;
+	private int anzahl;
+	private int buyin;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getStockname() {
+		return stockname;
+	}
+
+	public void setStockname(String stockname) {
+		this.stockname = stockname;
+	}
+
+	public int getAnzahl() {
+		return anzahl;
+	}
+
+	public void setAnzahl(int anzahl) {
+		this.anzahl = anzahl;
+	}
+
+	public int getBuyin() {
+		return buyin;
+	}
+
+	public void setBuyin(int buyin) {
+		this.buyin = buyin;
+	}
+
+}
--- servlets/InvestmentsServlet.java
+++ servlets/InvestmentsServlet.java
@@ -2,11 +2,20 @@ package stacked_bs.servlets;
 
 import jakarta.servlet.http.HttpServlet;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import jakarta.annotation.Resource;
 import jakarta.servlet.Servlet;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import stacked_bs.bean.Assets;
+import stacked_bs.bean.Login;
 
 /**
  * Tobias Weiß -> Hier ist noch nix geschehen wird es aber bald
@@ -22,7 +31,29 @@ public class InvestmentsServlet extends HttpServlet implements Servlet {
 		super();
 		// TODO Auto-generated constructor stub
 	}
+	
+	@Resource(lookup="java:jboss/datasources/MySqlThidbDS")
+	private DataSource ds;
 
+	private void persist(Assets assets) throws ServletException{
+		
+		try(Connection con = ds.getConnection(); 
+			PreparedStatement pstmt = con.prepareStatement(
+					"INSERT INTO investments (stockname) Values(?)"
+					)){
+			//pstmt.setString(1, username.getUsername());
+			pstmt.setString(1, assets.getStockname());
+		//	pstmt.setInt(2, assets.getAnzahl());
+		//	pstmt.setInt(3, assets.getBuyin());
+			pstmt.executeUpdate();
+			
+		} catch (SQLException e) {
+		
+			throw new ServletException(e.getMessage());
+		}
+	}
+	
+	
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
 	 *      response)
@@ -30,7 +61,21 @@ public class InvestmentsServlet extends HttpServlet implements Servlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.setCharacterEncoding("UTF-8");
+		
+		Assets assets = new Assets();
+	//	assets.setUsername(request.getParameter("username"));
+		assets.setStockname(request.getParameter("stockname"));
+	//hat noch probleme mit den Integern bin noch am fixen
+		//assets.setAnzahl(Integer.valueOf(request.getParameter("anzahl")));
+		//assets.setBuyin(Integer.valueOf(request.getParameter("buyin")));
+		
+		persist(assets);
+		
+		request.setAttribute("assets", assets);
+	
+		response.sendRedirect("Stacked/JSP/Profil.jsp");
+		
 	}
 
 	/**

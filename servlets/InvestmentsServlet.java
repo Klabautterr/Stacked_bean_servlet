a1fa67776b7ff60cbf1555179cea8931ec731343
wobias12 <tobias.weiss1@gmx.de>
2023-06-04 22:32:14 +0200

    Aenderung im Investment

--- servlets/InvestmentsServlet.java
+++ servlets/InvestmentsServlet.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServlet;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import javax.sql.DataSource;
@@ -14,6 +15,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import stacked_bs.bean.Assets;
 import stacked_bs.bean.Login;
 
@@ -31,51 +33,47 @@ public class InvestmentsServlet extends HttpServlet implements Servlet {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	
-	@Resource(lookup="java:jboss/datasources/MySqlThidbDS")
+
+	@Resource(lookup = "java:jboss/datasources/MySqlThidbDS")
 	private DataSource ds;
 
-	private void persist(Assets assets) throws ServletException{
-		
-		try(Connection con = ds.getConnection(); 
-			PreparedStatement pstmt = con.prepareStatement(
-					"INSERT INTO investments (stockname) Values(?)"
-					)){
-			//pstmt.setString(1, username.getUsername());
+	private void persist(Assets assets) throws ServletException {
+
+		try (Connection con = ds.getConnection();
+				PreparedStatement pstmt = con.prepareStatement("INSERT INTO thidb.investments (stockname) VALUES(?)")) {
+			// pstmt.setString(1, username.getUsername());
 			pstmt.setString(1, assets.getStockname());
-		//	pstmt.setInt(2, assets.getAnzahl());
-		//	pstmt.setInt(3, assets.getBuyin());
+			// pstmt.setInt(2, assets.getAnzahl());
+			// pstmt.setInt(3, assets.getBuyin());
 			pstmt.executeUpdate();
-			
+
 		} catch (SQLException e) {
-		
+
 			throw new ServletException(e.getMessage());
 		}
 	}
-	
-	
+
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
+
 		request.setCharacterEncoding("UTF-8");
-		
+
 		Assets assets = new Assets();
-	//	assets.setUsername(request.getParameter("username"));
+		// assets.setUsername(request.getParameter("username"));
 		assets.setStockname(request.getParameter("stockname"));
-	//hat noch probleme mit den Integern bin noch am fixen
-		//assets.setAnzahl(Integer.valueOf(request.getParameter("anzahl")));
-		//assets.setBuyin(Integer.valueOf(request.getParameter("buyin")));
-		
+		// hat noch probleme mit den Integern bin noch am fixen
+		// assets.setAnzahl(Integer.valueOf(request.getParameter("anzahl")));
+		// assets.setBuyin(Integer.valueOf(request.getParameter("buyin")));
+		HttpSession session = request.getSession();
+		session.setAttribute("Assets", assets);
 		persist(assets);
-		
-		request.setAttribute("assets", assets);
-	
+
 		response.sendRedirect("Stacked/JSP/Profil.jsp");
-		
+
 	}
 
 	/**

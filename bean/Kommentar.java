f4fa4bb2824cb0f0bff3fbe7aadb1a918eedad0a
Klabautter <68990372+Klabautter@users.noreply.github.com>
2023-06-18 16:20:45 +0200

    bean und servlet für kommentieren
    funktioniert noch ned

---
+++ bean/Kommentar.java
@@ -0,0 +1,40 @@
+package stacked_bs.bean;
+
+import java.io.Serializable;
+
+public class Kommentar implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private String username;
+	private String kommentar;
+	private Long id;
+	private Integer post_id;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getKommentar() {
+		return kommentar;
+	}
+	public void setKommentar(String kommentar) {
+		this.kommentar = kommentar;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Integer getPost_id() {
+		return post_id;
+	}
+	public void setPost_id(Integer post_id) {
+		this.post_id = post_id;
+	}
+	
+	
+
+}
---
+++ servlets/Kommentieren.java
@@ -0,0 +1,94 @@
+package stacked_bs.servlets;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.sql.DataSource;
+
+import stacked_bs.bean.Kommentar;
+import jakarta.annotation.Resource;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import jakarta.servlet.http.Part;
+import stacked_bs.bean.Login;
+import stacked_bs.bean.Post;
+
+/**
+ * Servlet implementation class Kommentieren
+ */
+@WebServlet("/Kommentieren")
+public class Kommentieren extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Resource(lookup = "java:jboss/datasources/MySqlThidbDS")
+	private DataSource ds;
+	
+	
+		private void ein_Kommentar(Kommentar formPost) throws ServletException {
+    	
+    	String[] DataInfo = new String[] {"id"};
+    	
+    	try (Connection con = ds.getConnection();
+    			// schreibt die Daten in die DAtenbank 
+    		PreparedStatement pstmt = con.prepareStatement(
+    		"INSERT INTO Kommentar (kommentar, username) Values(?, ?)", DataInfo)) {
+    		pstmt.setString(1, formPost.getKommentar());
+    		pstmt.setString(2, formPost.getUsername());
+    		//pstmt.setString(3, formPost.getBildname());
+    		//pstmt.setBinaryStream(4, filepart.getInputStream());
+    		pstmt.executeUpdate();
+    	
+    	try(ResultSet rs = pstmt.getGeneratedKeys()) {
+    		while (rs.next()) {
+    			formPost.setId(rs.getLong(1));
+    		}	
+    	}
+    	}catch (Exception ex) {
+    		throw new ServletException(ex.getMessage());
+    	}
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("UTF-8");
+		//zieht die daten aus der Login session
+		HttpSession session = request.getSession();
+		Login login = (Login) session.getAttribute("Login");
+	
+		
+		//holt die Daten aus der JSP und erstellt eine object bean
+		Kommentar formKommentar = new Kommentar();
+		formKommentar.setUsername(login.getUsername());
+		formKommentar.setKommentar(request.getParameter("kommentar"));
+	
+	
+		
+
+		session.setAttribute("formKommentar", formKommentar);
+		ein_Kommentar(formKommentar);
+
+
+		
+		response.sendRedirect("./AllePostsAusgeben");
+
+	
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}

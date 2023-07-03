279fa38abd2ac832acb1bf9f9a59843b88f9c3bb
Klabautter <68990372+Klabautter@users.noreply.github.com>
2023-07-03 12:38:42 +0200

    beans benannt welche meine

--- bean/Post.java
+++ bean/Post.java
@@ -1,3 +1,4 @@
+//Linus Baumeister
 package stacked_bs.bean;
 
 import java.io.Serializable;
@@ -8,8 +9,7 @@ public class Post implements Serializable{
 	private Long id;
 	private String username;
 	private String nachricht;
-	private String link;
-	private String loeschen;
+	
 	private String bildname;
 	private Integer anzahl_likes;
 	private byte[] bild;
@@ -21,17 +21,8 @@ public class Post implements Serializable{
 	public void setAnzahl_likes(Integer anzahl_likes) {
 		this.anzahl_likes = anzahl_likes;
 	}
-	public String isLoeschen() {
-		return loeschen;
-	}
-	public void setLoeschen(String string) {
-		this.loeschen = string;
-	}
 	public Long getId() {
 		return id;
-	}
-	public String getLoeschen() {
-		return loeschen;
 	}
 	public void setId(Long id) {
 		this.id = id;
@@ -48,12 +39,6 @@ public class Post implements Serializable{
 	public void setNachricht(String nachricht) {
 		this.nachricht = nachricht;
 	}
-	public String getLink() {
-		return link;
-	}
-	public void setLink(String link) {
-		this.link = link;
-	}
 	public String getBildname() {
 		return bildname;
 	}
@@ -68,3 +53,4 @@ public class Post implements Serializable{
 	}
 	
 }
+//Linus Baumeister
